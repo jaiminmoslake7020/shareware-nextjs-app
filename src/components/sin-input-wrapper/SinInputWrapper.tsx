2673fc66ebff227240609c1520d3efa728f62249
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-12-13 10:34:31 -0800

    correcting UX

--- src/components/sin-input-wrapper/SinInputWrapper.tsx
+++ src/components/sin-input-wrapper/SinInputWrapper.tsx
@@ -8,7 +8,17 @@ const SinInputWrapper = () => {
         <SinAppContextProvider>
             <div className={"sin-input-wrapper"}>
                 <RealInputBox />
+                <label className={"font-semibold"} >Please insert SSN</label>
                 <IllusionWrapper />
+                <div className={"flex flex-col gap-2 max-w-[350px] lg:max-w-[550px] mt-4"}>
+                    <span className={"text-xs text-gray-500 font-semibold"}>Notes</span>
+                    <ul className={"list-disc pl-6"}>
+                        <li className={"text-xs text-gray-500 font-semibold"}>SSN is 9 digit number.</li>
+                        <li className={"text-xs text-gray-500 font-semibold"}>When sum all numbers of SSN.</li>
+                        <li className={"text-xs text-gray-500 font-semibold"}>Double the every alternate number starting second number.</li>
+                        <li className={"text-xs text-gray-500 font-semibold"}>Final sum of all the number when divided by 10 will have a 0 as remainder.</li>
+                    </ul>
+                </div>
             </div>
         </SinAppContextProvider>
     );
